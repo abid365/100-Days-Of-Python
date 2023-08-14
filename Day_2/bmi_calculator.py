eb98f418a3cf203de53f7a4947bbbd725a14896a
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-14 11:40:43 +0600

    bmi calculator day-2

---
+++ Day_2/bmi_calculator.py
@@ -0,0 +1,7 @@
+# mathematical operation in python
+# pemdas left to right
+# Parenthesis Exponent Multipliction(same level), Division(same level), addition(same level), substraction(same level)
+height = float(input("Height: "))
+weight = float(input("Weight: "))
+bmi = weight / (height ** 2)
+print(int(bmi))
