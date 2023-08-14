5b81af0cc7ffd9f6b41aeb79640816d25ab25bcf
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-14 11:56:29 +0600

    summary

--- Day_2/bmi_calculator.py
+++ Day_2/bmi_calculator.py
@@ -1,7 +1,10 @@
 # mathematical operation in python
 # pemdas left to right
-# Parenthesis Exponent Multipliction(same level), Division(same level), addition(same level), substraction(same level)
+# Parenthesis Exponent Multiplication(same level), Division(same level), addition(same level), substraction(same level)
+# use python inbuilt <round> function to get rounded numbers
+# replacing int in line 8 with round
+# summary: <round> is used to get whole numbers / rounded numbers in py
 height = float(input("Height: "))
 weight = float(input("Weight: "))
 bmi = weight / (height ** 2)
-print(int(bmi))
+print(round(bmi))
