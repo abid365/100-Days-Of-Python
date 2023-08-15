57e5d9a664d2e87e02a3702b39d90413d8ad7a19
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-15 11:00:58 +0600

    your life in weeks

--- Day_2/bmi_calculator.py
+++ Day_2/bmi_calculator.py
@@ -3,8 +3,11 @@
 # Parenthesis Exponent Multiplication(same level), Division(same level), addition(same level), substraction(same level)
 # use python inbuilt <round> function to get rounded numbers
 # replacing int in line 8 with round
-# summary: <round> is used to get whole numbers / rounded numbers in py
+# summary: <round> is used to get whole numbers / decimal places
+# rounded(value, decimal places tobe executed)
+# on line 12 the value will be printed rounded to two decimal places
 height = float(input("Height: "))
 weight = float(input("Weight: "))
 bmi = weight / (height ** 2)
 print(round(bmi))
+print(round(8 / 3, 2))
