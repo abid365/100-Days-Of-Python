ee62a93c7c0334b57765f90084fb03df6a88955b
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-18 22:16:55 +0600

    setup completed

---
+++ Day_3/love_calculator.py
@@ -0,0 +1,8 @@
+yourName = input("Type your name: ")
+theirName = input("Type their name: ")
+# True love
+yourCount = 0
+if yourName.lower().count("t"):
+    yourCount += yourName.lower().count("t")
+
+print(yourCount)
