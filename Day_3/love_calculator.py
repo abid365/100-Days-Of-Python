db89a03c8431a6d00766699a22e408cdb79c182e
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-18 23:12:44 +0600

    logic created

--- Day_3/love_calculator.py
+++ Day_3/love_calculator.py
@@ -1,8 +1,19 @@
 yourName = input("Type your name: ")
 theirName = input("Type their name: ")
 # True love
-yourCount = 0
-if yourName.lower().count("t"):
-    yourCount += yourName.lower().count("t")
+lowerCaseString = (yourName + theirName).lower()
+t = lowerCaseString.count("t")
+r = lowerCaseString.count("r")
+u = lowerCaseString.count("u")
+e = lowerCaseString.count("e")
+true = t + r + u + e
 
-print(yourCount)
+l = lowerCaseString.count("l")
+o = lowerCaseString.count("o")
+v = lowerCaseString.count("v")
+e = lowerCaseString.count("e")
+love = l + o + v + e
+
+love_score = str(true) + str(love)
+print(love_score)
+
