2cf2fe7a0074e6eb101a135fe08b8633e9e3769d
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-18 23:36:09 +0600

    exercise done

--- Day_3/love_calculator.py
+++ Day_3/love_calculator.py
@@ -15,5 +15,11 @@ e = lowerCaseString.count("e")
 love = l + o + v + e
 
 love_score = str(true) + str(love)
-print(love_score)
+lvScoreInteger = int(love_score)
 
+if lvScoreInteger < 10 or lvScoreInteger > 90:
+    print(f"Your score is {lvScoreInteger} you go together like coke and mentos")
+elif lvScoreInteger <= 40 or lvScoreInteger >= 50:
+    print(f"Your score is {lvScoreInteger}, you are alright together")
+else:
+    print(f"Your score is {lvScoreInteger}")
