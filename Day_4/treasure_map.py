39e76c3b70b4afe38a3f59e5fac7ecdf8b84e927
jawad Bin Azam <abidibnazam@gmail.com>
2023-10-24 13:52:27 +0600

    final push

--- Day_4/treasure_map.py
+++ Day_4/treasure_map.py
@@ -14,5 +14,6 @@ letter = position[0].lower()
 abc = ["a", "b", "c"]
 letter_index = abc.index(letter)
 number_index = int(position[1]) - 1
+map[number_index][letter_index] = "X"
 
 print(f"{line1}\n{line2}\n{line3}")
